5d91726edd7684e1becfba83af50791ef6f5a346
Guillaume Claret <dev@clarus.me>
2024-11-15 16:50:41 +0100

    Deploy website - based on 42fc69330683da633d9f2af6a67cdb89343ba9c0

---
+++ assets/js/28295219.69a51e2e.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkformal_land=self.webpackChunkformal_land||[]).push([[4557],{5700:a=>{a.exports=JSON.parse('{"label":"Noir","permalink":"/blog/tags/noir","allTagsPath":"/blog/tags","count":2,"unlisted":false}')}}]);
