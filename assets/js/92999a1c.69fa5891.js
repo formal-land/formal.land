b8b3ab96c2959ddb5723c7bc8d8523487166913d
Guillaume Claret <dev@clarus.me>
2024-12-26 21:32:45 +0100

    Deploy website - based on 7df641eef2c49fc17e6bba312ab533d130541150

--- assets/js/92999a1c.69fa5891.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkformal_land=self.webpackChunkformal_land||[]).push([[8790],{8735:e=>{e.exports=JSON.parse('{"permalink":"/blog/page/3","page":3,"postsPerPage":10,"totalPages":5,"totalCount":45,"previousPage":"/blog/page/2","nextPage":"/blog/page/4","blogDescription":"Blog","blogTitle":"Blog"}')}}]);
